1b458e46ff11d2510a561fce71a468fef059fe5b
Sarah Tolley <stolley@ucla.edu>
2015-02-03 06:24:46 +0000

    add for loop to proccess DNA in input.txt file

--- Ch4_ex1.py
+++ Ch4_ex1.py
@@ -4,3 +4,10 @@ file = open("input.txt")
 #create output file
 output = open("trimmed_dna.txt", "w")
 
+#create for loop
+for dna in file:
+	trimmed_dna = dna[14:]
+	#print the trimmed dna sequence
+	output.write(trimmed_dna)
+	#print the length of the trimmed dna to the screen
+	print("processed sequence length " + str(len(trimmed_dna)))
